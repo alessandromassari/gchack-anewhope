020cfe45e07f662f07a4ac94842b7263944418cb
ale.massari <105865898+alessandromassari@users.noreply.github.com>
2025-05-29 22:38:51 +0200

    Update my_model.py new hope network

--- source/my_model.py
+++ source/my_model.py
@@ -4,16 +4,15 @@ import torch.nn.functional as F
 from torch_geometric.nn import GINConv, NNConv, global_mean_pool, global_max_pool, global_add_pool, JumpingKnowledge
 
 
-# GIN network section
-class myGIN(nn.Module):
+# New hope network (we cannot do just ctrl+C and ctrl+V, we're still young and hungry
+class NHNmodel(nn.Module):
     def __init__(self, node_in_dim: int = 1,
                  edge_in_dim: int = 7,
                  hidden_dim: int =64, # try with 128
                  num_layers: int = 4,
-                 jk_mode: str = "cat",
                  mlp_classifier_dims=(384, 256, 128),
                  out_classes: int = 6,
-                 dropout: float = 0.4):
+                 dropout: float = 0.3):
         #
         super().__init__()
         self.num_layers = num_layers
@@ -22,24 +21,24 @@ class myGIN(nn.Module):
         self.convs = nn.ModuleList()
         for i in range(num_layers):
             in_ch = node_in_dim if i == 0 else hidden_dim
-            #MLP for edge weights
-            nn_edge = nn.Sequential(
-                nn.Linear(in_ch, hidden_dim),
+            out_ch = hidden_dim
+            
+            #MLP for edge weights: a sort of encoder in my mind
+            nn_edge_mlp = nn.Sequential(
+                nn.Linear(edge_in_dim, hidden_dim),
                 nn.ReLU(),
-                nn.Linear(hidden_dim, hidden_dim)
+                nn.Linear(hidden_dim, in_ch*hidden_dim)
             )
-            conv = GINConv(nn_edge,  train_eps=True, aggr='sum')
+            # NNConv 
+            conv = NNConv(in_ch, out_ch, nn_edge_mlp, aggr='sum')
             self.convs.append(conv)
 
-        #jumping knowledge aggregator
-        self.jk = JumpingKnowledge(mode=jk_mode)
-
-        # pooling stage
+        # pooling stage - to choose one - try and then choose
         self.pool_fns = [global_mean_pool, global_max_pool, global_add_pool]
 
         # final MLP classifier
         mlp_layers = []
-        input_dim = hidden_dim * num_layers * len(self.pool_fns) if jk_mode == 'cat' else hidden_dim * len(self.pool_fns)
+        input_dim = hidden_dim * len(self.pool_fns)
         prev_dim = input_dim
         for dim in mlp_classifier_dims:
             mlp_layers += [
@@ -58,20 +57,18 @@ class myGIN(nn.Module):
         h_list = []
         h = x
         for conv in self.convs:
-            h = conv(h, edge_index)
+            h = conv(h, edge_index, edge_attr)
             h = F.relu(h)
             h = F.dropout(h, p=self.drop_p, training=self.training)
             h_list.append(h)
 
-        # apply Jumping Knowledge
-        h_jk = self.jk(h_list)  # [N, hidden_dim * num_layers] if cat, else [N, hidden_dim]
-
-        # global pooling
+        # final pooling stage: MLP classifier need a
+        h_final = h_list[-1]
         pooled = []
         for pool in self.pool_fns:
-            pooled.append(pool(h_jk, batch))  # each [batch_size, hidden_dim * num_layers] or [batch_size, hidden_dim]
-        graph_repr = torch.cat(pooled, dim=-1)  # [batch_size, hidden_dim * num_layers * 3] or [batch_size, hidden_dim * 3]
-
+            pooled.append(pool(h_final, batch)) # Pooling su h_final
+        graph_repr = torch.cat(pooled, dim=-1)
+        
         # classification
         out = self.classifier(graph_repr)  # [batch_size, out_classes]
         return out
